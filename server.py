f5c9ad9a0534790a9028bad88c0419636c57a81e
Travis Roth <travisro@outlook.com>
2024-02-15 17:20:20 -0800

    added print json to server logs to debug

--- server.py
+++ server.py
@@ -207,8 +207,10 @@ def website():
     for session in sessions:
         response = DbIpCity.get(session.ip_address, api_key='free')
         user_locations.append({'session_id': session.id, 'lat': response.latitude, 'lng': response.longitude})
-    # Convert fetched data into a Pandas DataFrame
 
+    print("Page Views JSON:", page_views_json)
+    print("Daily Users JSON:", users_per_day_json)
+    print("User Locations JSON:", user_locations)
 
     logger.debug('Rendering website page')
     return render_template('/website.html', page_views=page_views_json, daily_users=users_per_day_json, user_locations=user_locations)
